623b739e632e74de0ba7aa5fba48d00af9e8073e
Kristian Os <codedevstem@gmail.com>
2016-09-26 13:25:25 +0200

    Updated some files

--- INFO100/solutions/lab4/scripts/main.js
+++ INFO100/solutions/lab4/scripts/main.js
@@ -2,9 +2,7 @@ function visSjulTekst() {
   var textItem = document.getElementById("text");
   if (textItem.style.display === "none") {
     textItem.style.display = "block";
-
   }else {
     textItem.style.display = "none";
-
   }
 }
