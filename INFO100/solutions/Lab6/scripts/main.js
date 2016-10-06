20a1d7581293a5a9a96253f8fbafd9a463bde991
Kristian Os <codedevstem@gmail.com>
2016-10-06 16:33:50 +0200

    created solutions and added on the css ass

---
+++ INFO100/solutions/Lab6/scripts/main.js
@@ -0,0 +1,42 @@
+var footerText = `
+  <p>This page is created by Kristian Os</p>
+  <p><a href="mailto:kos003@student.uib.no?subject=feedback"> Send me a mail to give feedback.</a>
+  <a href="#" onclick="changeText()">Click her for norsk informasjon.</a></p>
+`;
+function changeText() {
+  var temp = document.getElementById('footer').innerHTML;
+  document.getElementById('footer').innerHTML = footerText;
+  footerText = temp;
+}
+function showHide(id) {
+  var elem = document.getElementById(id);
+  if (elem.style.display === "block") {
+    elem.style.display = "none";
+  } else {
+    elem.style.display = "block";
+  }
+}
+
+function openFastRegPage(url) {
+  window.open(url);
+}
+
+function generateJobs(xml) {
+
+  var i;
+  var xmlDoc = xml.responseXML;
+  var jobs="<h1>Resultatsiden for jobber som er evaluert.</h1>";
+  var x = xmlDoc.getElementsByTagName("JOB");
+  for (i = 0; i <x.length; i++) {
+    jobs += "<section class='result'><h2>" +
+    x[i].getElementsByTagName("POSITION")[0].childNodes[0].nodeValue +
+    "<button type='button' onclick='showHide(`job" + (i+1) +"Info`)'>Vis mer info</button></h2>" +
+    "<span id='job" + (i+1) +"Info'>" +
+    "<p>" + x[i].getElementsByTagName("WORKPLACE")[0].childNodes[0].nodeValue +
+    "</p><p>" + x[i].getElementsByTagName("EMPLOYEER")[0].childNodes[0].nodeValue +
+    "</p><p>" + x[i].getElementsByTagName("DATEREGISTERED")[0].childNodes[0].nodeValue +
+    "</p><p>" + x[i].getElementsByTagName("DEADLINE")[0].childNodes[0].nodeValue +
+    "</p></span></section>";
+  }
+  document.getElementById("allJobs").innerHTML = jobs;
+}
