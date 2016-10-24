07f14832a2643bafd148b8acf70728f3692891b1
Kristian Os <codedevstem@gmail.com>
2016-10-24 18:12:35 +0200

    reset

--- dikult105/css_assignment/scripts/switchCSS.js
+++
@@ -1,6 +0,0 @@
-function switchCSS(first, second) {
-  var styleLink = document.getElementById("replaceLink");
-  var href = styleLink.getAttribute("href");
-  newHref = href == first ? second : first;
-  styleLink.setAttribute("href", newHref);
-}  
