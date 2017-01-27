fbe3d5939429b66d07fa4ca028ebcd50b868240b
Kristian Os <codedevstem@gmail.com>
2017-01-27 11:06:49 +0100

    Lab 1 Dikult205 testing

---
+++ dikult205/labs/lab1/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <title></title>
+        <link rel='stylesheet' src=''>
+    </head>
+    <body>
+        <header>
+                    
+        </header>
+        <main>
+                    
+        </main>
+        <footer>
+                    
+        </footer>
+    </body>
+</html>
