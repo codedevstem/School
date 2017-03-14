3db7deb501c83bc688fe93bda23a68f0d3c6d948
Kristian Os <codedevstem@gmail.com>
2017-03-14 20:11:31 +0100

    Created jsDocs

--- info134/ass2/script/show_movie.js
+++ info134/ass2/script/show_movie.js
@@ -1,11 +1,16 @@
 let movie;
 let movieGenres;
 let movieAvgRating;
+let descFlag;
+let query_params;
+
 function panic(message) {
     // window.history.back();
     alert(message);
 }
-
+/**
+ * @description | Started on finilization of the HTML. 
+ */
 window.onload = function() {
     query_params = get_query_string_parameters();
     if (!query_params.id) {
@@ -14,14 +19,7 @@ window.onload = function() {
     }else{
         makeMovieArray();
         getMovieIdAndGenre();
-        
-        getElemId('oTitle').innerHTML = movie.otitle;
-        if(movie.etitle != null && movie.etitle != movie.otitle){
-            getElemId('eTitle').innerHTML = movie.etitle;
-        }
-        if(movie.ntitle != null && movie.ntitle != movie.otitle){
-            getElemId('nTitle').innerHTML = movie.ntitle;
-        }
+        createTitleHeaders();
         createButtonsSection(getElemId("infoSection"));
         createDetailedInfoSection(getElemId("detailedInfo"));
         if(movieAvgRating == null){
@@ -30,51 +28,78 @@ window.onload = function() {
             getElemId("currentRating").innerHTML = "Andre har gitt denne filmen: " + movieAvgRating.toFixed(1);
         }
         getElemId("submitRating").addEventListener("click", function(){giveRatingToMovie()}, false);
-        createMoreInfoSection(getElemId("moreInfo"));
-        let youtubeElem = document.createElement("iframe");
-        youtubeElem.height = "315";
-        youtubeElem.src = "https://www.youtube.com/embed/"+movie.youtube_trailer_id;
-        youtubeElem.allowFullscreen = true;
-        getElemId("trailer").appendChild(youtubeElem);
+        //Tests for movies with youtube link. 
+        if(movie.id == "3823" || movie.id == "3818" || movie.id == "1024" || movie.id == "3597" || movie.id == "2274" ||
+        movie.id == "2754" || movie.id == "46" || movie.id == "3308" || movie.id == "3429" || movie.id == "83" ) {
+            let youtubeElem = document.createElement("iframe");
+            youtubeElem.height = "315";
+            youtubeElem.src = "https://www.youtube.com/embed/"+movie.youtube_trailer_id;
+            youtubeElem.allowFullscreen = true;
+            getElemId("trailer").appendChild(youtubeElem);
+        }
+        //Creates images b,c and d. Just in case
         getElemId("images").appendChild(createImageElem("b"));
         getElemId("images").appendChild(createImageElem("c"));
+        getElemId("images").appendChild(createImageElem("d"));
     }
     
 };
+/**
+ * @description | inserts the movie title on top of document 
+ */
+function createTitleHeaders() {
+    getElemId('oTitle').innerHTML = movie.otitle;
+    if(movie.etitle != null && movie.etitle != movie.otitle){
+        getElemId('eTitle').innerHTML = movie.etitle;
+    }
+    if(movie.ntitle != null && movie.ntitle != movie.otitle){
+        getElemId('nTitle').innerHTML = movie.ntitle;
+    }
+}
+/**
+ * @description | Creates and image node, assigns a src and alt property and also
+ * assigns an onerror event that hides the image if it can not be loaded. 
+ * 
+ * @param {String} letter | Indicator of the image to get from the URL. 
+ * 
+ * @returns {Node} image | an image node to be appended onto its parent.
+ */
 function createImageElem(letter){
-    image = document.createElement("img");
+    let image = document.createElement("img");
     image.src = "https://nelson.uib.no/o/" + parseInt(movie.id / 1000) + "/" + movie.id + letter + ".jpg";
     image.alt = "Cover image of " + movie.otitle;
     image.onerror = ()=>{image.style.display = "none"};
     return image;
 }
-function createMoreInfoSection(parent) {
-    parent.appendChild(createInfoElem("infoHeader", "Handling"));
-    parent.appendChild(createInfoElem("explanation", movie.description));
-    parent.appendChild(createInfoElem("infoHeader", "Produsert"));
-    parent.appendChild(createInfoElem("explanation", movie.country));
-    parent.appendChild(createInfoElem("infoHeader", "regissører"));
-    parent.appendChild(createInfoElem("explanation", movie.dir));
-}
+/**
+ * @description | Gets the movie id of from the object.json file, 
+ * Compares it to genres.json to find if it has any genres. 
+ * Compares it to reviews.json to find if it has been rated
+ * If it has been rated it is then calculated.
+ */
 function getMovieIdAndGenre() {
     movieArray.forEach(movies => {
         if(movies.id == query_params.id) {
             movie = movies;
-            for(genreMovie in genres_object){
+            // check for genres and collect them
+            for(let genreMovie in genres_object){
                 if(genreMovie == query_params.id){
                     movieGenres = genres_object[genreMovie].toString().replace(/\,/g, ", ");
                 }
             }
             let totalRating = 0;
             let numberOfRating = 0;
-            for (review in reviews_object) {
+            //check for ratings and collect data
+            for (let review in reviews_object) {
                 if (review == movie.id) {
-                    for (user in reviews_object[review]) {
+                    for (let user in reviews_object[review]) {
                         totalRating += reviews_object[review][user].rating;
                         numberOfRating++;
                     }
                 }
 		    }
+            /* checks if it is rated and calculates it 
+                else it is given a null value */
             if(numberOfRating > 0){
                 movieAvgRating = totalRating / numberOfRating
             } else {
@@ -84,21 +109,47 @@ function getMovieIdAndGenre() {
         }
     });
 }
-function toggleChopList() {
+/**
+ * @description | Event to be fired when someone clicks the button to add to take list.
+ */
+function toggleTakeList() {
     console.log("added to take list");
 }
+/**
+ * @description | Event to be fired when someone clicks the button to add to wish list.
+ */
 function toggleWishList(){
     console.log("added to wish list!");
 }
+/**
+ * @description | Shorthand function to retrive a node
+ * @param {String} id | The id of the element you want
+ * @returns {Node} 
+ */
 function getElemId(id) {
     return document.getElementById(id);
 }
-function createInfoElem(className, content) {
+/**
+ * @description | Creates a list element to be placed into a list
+ * 
+ * @param {String} className | Name of the class the list item should have
+ * @param {String} content | The content to be placed into innerHTML.
+ * @param {String} id | ? | Not needed but assigns an id to the node if recieved
+ * 
+ * @returns {Node} newElem | a <li> node
+ */
+function createInfoElem(className, content, id = "") {
     let newElem = document.createElement("li");
     newElem.classList.add(className);
+    newElem.id = id;
     newElem.innerHTML = content;
     return newElem;
 }
+/**
+ * @description | Function to append child nodes to the <ul> parent node
+ * 
+ * @param {Node} parent | the parent node to place all created nodes into.
+ */
 function createDetailedInfoSection(parent) {
     parent.appendChild(createInfoElem("infoHeader", "Genre"));
     parent.appendChild(createInfoElem("explanation", movieGenres));
@@ -108,17 +159,47 @@ function createDetailedInfoSection(parent) {
     // Special Case to create the list of actors.
     let actorList = document.createElement("ul");
     actorList.classList.add("explanation");
+    // Splits the actors into items
     movie.folk.split(", ").forEach(actors => {
         let actorLink = document.createElement("a");
         actorLink.href = "";
+        // Removes trailing commas
         actorLink.innerHTML = actors.split(',', 1);
         actorList.appendChild(actorLink);
     });
     parent.appendChild(actorList);
 
-    parent.appendChild(createInfoElem("infoHeader", "Kort Sammendrag"));
-    parent.appendChild(createInfoElem("explanation", movie.description.substring(0,180) + "..."));
+    parent.appendChild(createInfoElem("infoHeader", "Kort Sammendrag", "descriptionHeader"));
+    // Special case to create description since it must be expandable.  
+    let description = createInfoElem("explanation", "", "description");
+    // Adding the eventListener.
+    description.addEventListener("click", function(){toggleDescLength()}, false);
+    parent.appendChild(description);
+    
+    parent.appendChild(createInfoElem("infoHeader", "Produsert"));
+    parent.appendChild(createInfoElem("explanation", movie.country));
+    parent.appendChild(createInfoElem("infoHeader", "regissører"));
+    parent.appendChild(createInfoElem("explanation", movie.dir));
+    toggleDescLength();
 }
+/**
+ * @description | Toggles between long and short description for the current movie.
+ */
+function toggleDescLength() {
+    if(descFlag) {
+        getElemId("descriptionHeader").innerHTML = "Langt Sammendrag";
+        getElemId("description").innerHTML = movie.description;
+    } else {
+        getElemId("descriptionHeader").innerHTML = "Kort Sammendrag";        
+        getElemId("description").innerHTML = movie.description.substring(0,180) + "... Trykk for å lese mer";
+    }
+    // Toggles the description flag. 
+    descFlag = !descFlag;
+}
+/**
+ * @description | Creates a <div> to place buttons in and creates two buttons in that <div>
+ * @param {Node} parent | The parent node to append created child nodes to.
+ */
 function createButtonsSection(parent) {
     let buttonSection = document.createElement("div");
     buttonSection.classList.add("buttonSection");
@@ -126,7 +207,7 @@ function createButtonsSection(parent) {
     addToListButton.classList.add("addToList");
     addToListButton.type = "submit";
     addToListButton.innerHTML = "Add to cart";
-    addToListButton.addEventListener("click", toggleChopList)
+    addToListButton.addEventListener("click", toggleTakeList)
     let addToWishButton = document.createElement("button");
     addToWishButton.classList.add("addToWish");
     addToWishButton.type = "submit";
@@ -136,6 +217,10 @@ function createButtonsSection(parent) {
     buttonSection.appendChild(addToWishButton);
     parent.appendChild(buttonSection);
 }
+/**
+ * @description | Fires when a user submits a rating
+ * Does nothing yet, but will be implemented in the future.
+ */
 function giveRatingToMovie(){
     if(getElemId("star5").checked){
         console.log("5");
