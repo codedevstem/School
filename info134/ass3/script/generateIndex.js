8630fd39857720b2f46be1794f0ab909067e62e9
Kristian Os <codedevstem@gmail.com>
2017-05-03 10:31:53 +0200

    Added comment to functions

--- info134/ass3/script/generateIndex.js
+++ info134/ass3/script/generateIndex.js
@@ -15,62 +15,65 @@ makeMovieArray();
 pushToArray(newMoviesArray);
 pushToArray(recentMoviesArray);
 pushToArray(suggestionMoviesArray);
+// Calls 
 window.onload = function() {
 
-document.getElementById('newBack').style.opacity = "0";
-document.getElementById('recentBack').style.opacity = "0";
-document.getElementById('suggestedBack').style.opacity = "0";
-reviews = reviews_object;
-calculateAvgRatingAll();
-windowWidth = document.body.clientWidth;
-chooseNumberOfMovies();
-bindArrowClicks();
+	document.getElementById('newBack').style.opacity = "0";
+	document.getElementById('recentBack').style.opacity = "0";
+	document.getElementById('suggestedBack').style.opacity = "0";
+	reviews = reviews_object;
+	calculateAvgRatingAll();
+	windowWidth = document.body.clientWidth;
+	chooseNumberOfMovies();
+	bindArrowClicks();
 
 
-document.getElementById('submitRating').addEventListener("click", function(){
-	fillInRatingSection();
-}, false);
-// Filtering and sorting new movies array
-for(let i = newMoviesArray.length-1; i>=0; i--){
-		let movie = newMoviesArray[i];
-		if(movie.reg_date == null) newMoviesArray.splice(newMoviesArray.indexOf(movie), 1);
-	}
-newMoviesArray.sort(function(a,b){
-	let aDate = a.reg_date.substring(0, 4) + '' + a.reg_date.substring(5,7) + '' + a.reg_date.substring(8,10);
-	let bDate = b.reg_date.substring(0, 4) + '' + b.reg_date.substring(5,7) + '' + b.reg_date.substring(8,10);
-	return bDate - aDate
-});
-
-// Filling in new movies in the document
-appendList(newMoviesArray, 'newMovies', newMoviesIndex, numberOfMovies);
-// Filter out movies that are unlikely to have a picture... not sure how to fix this properly ((Todo?))
-filterMovies(recentMoviesArray);
-filterMovies(suggestionMoviesArray);
-
-
-//Shuffeling the movies so they are in random order((This should be based on recent movies, but that is not yet available to me))
-recentMoviesArray = shuffle(recentMoviesArray);
-
-//Sorting the movies so they are in sorted in order of rating
-suggestionMoviesArray.sort(function(a,b){
-	let aRating;
-	let bRating;
-	movieRatingArray.forEach(pair => {
-		if(pair["movieId"] == a.id) aRating = pair["rating"]; 
-		if(pair["movieId"] == b.id) bRating = pair["rating"]; 
+	document.getElementById('submitRating').addEventListener("click", function(){
+		fillInRatingSection();
+	}, false);
+	// Filtering and sorting new movies array
+	for(let i = newMoviesArray.length-1; i>=0; i--){
+			let movie = newMoviesArray[i];
+			if(movie.reg_date == null) newMoviesArray.splice(newMoviesArray.indexOf(movie), 1);
+		}
+	newMoviesArray.sort(function(a,b){
+		let aDate = a.reg_date.substring(0, 4) + '' + a.reg_date.substring(5,7) + '' + a.reg_date.substring(8,10);
+		let bDate = b.reg_date.substring(0, 4) + '' + b.reg_date.substring(5,7) + '' + b.reg_date.substring(8,10);
+		return bDate - aDate
 	});
-	
-	return bRating - aRating;
-}); 
+
+	// Filling in new movies in the document
+	appendList(newMoviesArray, 'newMovies', newMoviesIndex, numberOfMovies);
+	// Filter out movies that are unlikely to have a picture... not sure how to fix this properly ((Todo?))
+	filterMovies(recentMoviesArray);
+	filterMovies(suggestionMoviesArray);
 
 
-// Removing all the other element than the seven first in the list
-// Filling in movies in the sections
-appendList(recentMoviesArray, 'recentMovies', recentMoviesIndex, numberOfMovies)
-appendList(suggestionMoviesArray, 'suggestedMovies', suggestionMoviesIndex, numberOfMovies)
-fillInRatingSection();
+	//Shuffeling the movies so they are in random order((This should be based on recent movies, but that is not yet available to me))
+	recentMoviesArray = shuffle(recentMoviesArray);
+
+	//Sorting the movies so they are in sorted in order of rating
+	suggestionMoviesArray.sort(function(a,b){
+		let aRating;
+		let bRating;
+		movieRatingArray.forEach(pair => {
+			if(pair["movieId"] == a.id) aRating = pair["rating"]; 
+			if(pair["movieId"] == b.id) bRating = pair["rating"]; 
+		});
+		
+		return bRating - aRating;
+	}); 
+
+
+	// Removing all the other element than the seven first in the list
+	// Filling in movies in the sections
+	appendList(recentMoviesArray, 'recentMovies', recentMoviesIndex, numberOfMovies)
+	appendList(suggestionMoviesArray, 'suggestedMovies', suggestionMoviesIndex, numberOfMovies)
+	fillInRatingSection();
 }
-
+/**
+	* Fills the rating section with a movie and information about the movie.
+	*/
 function fillInRatingSection(){
 	let movie = randomPick(movieArray);
 	const rateMovie = document.getElementById('rateMovie');
@@ -212,6 +215,10 @@ function changeWindowValues() {
 	appendList(recentMoviesArray, 'recentMovies', recentMoviesIndex, numberOfMovies)
 	appendList(suggestionMoviesArray, 'suggestedMovies', suggestionMoviesIndex, numberOfMovies)
 }
+/**
+	* Takes the width of the screen and uses this information to decide how many
+	* movies should be shown for each "step" in the lists.
+	*/
 function chooseNumberOfMovies() {
  if(windowWidth < 840) {
 		numberOfMovies = Math.floor(windowWidth/160);
@@ -219,12 +226,19 @@ function chooseNumberOfMovies() {
 		numberOfMovies = Math.floor(windowWidth/200);
 	}
 }
+/**
+	* Function for clearing the lists so that new information can be shown.
+	*/
 function clearSections() {
 	document.getElementById('newMovies').innerHTML = '';
 	document.getElementById('recentMovies').innerHTML = '';
 	document.getElementById('suggestedMovies').innerHTML = '';
 
 }
+/**
+	* 
+	* @param {String} id | Id of the list to increase the numbering 
+	*/
 function increase(id) {
 	if(id == "newMovies"){
 		newMoviesIndex = increaseIndex(newMoviesIndex,newMoviesArray, 'newMovies');
